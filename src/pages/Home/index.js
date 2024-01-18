82defd3135234eece0b2173c6166f6d0b093a35b
Junno Nobrega <junno.nobrega@gmail.com>
2024-01-18 19:00:12 -0300

    feat: "Criação do campo para mostrar o perfil, e criação do componente para exibir os repositórios."

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import {Header} from '../../components/Header'
 import bakcground from '../../assets/background.png'
-
+import profile from '../../assets/profile-pic (1).png'
 import './styles.css'
+
+import ItemList from '../../components/ItemList'
 
 function App() {
   return (
@@ -13,6 +15,21 @@ function App() {
                 <div>
                     <input name="usuário" placeholder='@username' />
                     <button>Buscar</button>
+                </div>
+                <div className='perfil'>
+                    <img src ={profile} className='profile' alt="Foto do perfil" />
+                      
+                    <div > 
+                        <h3>Junno nóbrega</h3>
+                       <span>@junnons</span>
+                        <p>Descrição</p>
+                    </div>
+                </div>
+                <hr/>
+
+                <div>
+                  <h4 className='repositorio'>Repositórios</h4>
+                  <ItemList title="Teste1" description='Teste repositorio '/> 
                 </div>
             </div>
         </div>
